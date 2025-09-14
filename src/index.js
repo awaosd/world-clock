4aa06a19e2dd23ff6aab3754a3ee4b3d2c920400
awaosd <osunadiaz.eva@gmail.com>
2025-09-14 14:09:55 +0100

    added java for select

--- src/index.js
+++ src/index.js
@@ -5,11 +5,36 @@ setInterval(function () {
   let parisTime = document.querySelector("#time-paris");
   parisTime.innerHTML = `${moment().tz("Europe/Paris").format("HH:mm:ss")}`;
 
-  let berlinDate = document.querySelector("#date-berlin");
-  berlinDate.innerHTML = `${moment()
-    .tz("Europe/Berlin")
+  let hongkongDate = document.querySelector("#date-hongkong");
+  hongkongDate.innerHTML = `${moment()
+    .tz("Asia/Hong_Kong")
     .format("MMMM Do YYYY")}`;
 
-  let berlinTime = document.querySelector("#time-berlin");
-  berlinTime.innerHTML = `${moment().tz("Europe/Berlin").format("HH:mm:ss")}`;
+  let hongkongTime = document.querySelector("#time-hongkong");
+  hongkongTime.innerHTML = `${moment()
+    .tz("Asia/Hong_Kong")
+    .format("HH:mm:ss")}`;
+
+  let alaskaDate = document.querySelector("#date-alaska");
+  alaskaDate.innerHTML = `${moment().tz("US/Alaska").format("MMMM Do YYYY")}`;
+
+  let alaskaTime = document.querySelector("#time-alaska");
+  alaskaTime.innerHTML = `${moment().tz("US/Alaska").format("HH:mm:ss")}`;
 }, 1000);
+
+function showCity(event) {
+  let timeZone = event.target.value;
+  let cityName = timeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(timeZone);
+  let cities = document.querySelector("#cities");
+  cities.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div>
+            <div class="time">${cityTime.format("HH:mm:ss")}</div>
+          </div>`;
+}
+let citybutton = document.querySelector("#button");
+citybutton.addEventListener("change", showCity);
